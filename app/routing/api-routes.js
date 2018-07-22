0222ab822912a3865a148ac830034419ab731c06
MCPolichette <mark@polichette.com>
2018-07-22 00:17:01 -0600

    changed inputs to sliders

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -23,27 +23,19 @@ module.exports = function (app) {
             // for-loop goes through current friends data to compare scores
             for (var i = 0; i < friend_data.length; i++) {
                 var score_difference = 0;
-                console.log(friend_data[i].scores)
-
                 for (j = 0; j < user_scores.length; j++) {
                     score_difference = score_difference + (Math.abs(parseInt(friend_data[i].scores[j]) - parseInt(user_scores[j])));
-                    console.log("user" + user_scores[j]);
-                    console.log(score_difference + "scoredifference");
                 };
-
                 score_array.push(score_difference);
-                console.log(score_array + " SCORE ARRAY")
             }
             for (var i = 0; i < score_array.length; i++) {
                 if (score_array[i] <= score_array[best_match]) {
                     best_match = i;
                 }
             }
-            console.log(best_match)
+
             return best_match;
         }
-
-
         var bestFriend = friend_data[bestMatch()]
         res.json(bestFriend);
         friend_data.push(user);
